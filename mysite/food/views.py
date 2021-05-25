575b71c243e54c7c65bfc5f639c3592589578490
rr2x <79620740+rr2x@users.noreply.github.com>
2021-05-25 23:42:52 +0800

    Database & Models In Django (2), Templates(1)

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    item_list = Item.objects.all()
+    context = {
+        'item_list': item_list,
+    }
+    return render(request, 'food/index.html', context)
 
 
 def anotherlink(request):
