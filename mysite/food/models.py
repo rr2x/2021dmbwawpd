3036e28553e111fbf2c1e988843ee65ebc6c0c80
rr2x <79620740+rr2x@users.noreply.github.com>
2021-05-26 11:41:33 +0800

    Static Files & Site Design (2)

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -9,3 +9,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500, default='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.WbhJPOi8PiEDjZuFpEthMwHaE8%26pid%3DApi&f=1')
