b5a7c1dcbd87c5f061fae20f5aad9b89af4b2392
rr2x <79620740+rr2x@users.noreply.github.com>
2021-05-26 08:30:50 +0800

    Templates(2), Static Files & Site Design (1)

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
 
+app_name = 'food'  # namespacing, to prevent other apps from using the same url path
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello/', views.anotherlink, name='anotherlink'),
-    path('item/', views.item, name='item')
+    path('item/', views.item, name='item'),
+    path('<int:item_id>', views.detail, name='detail')
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -17,3 +17,12 @@ def anotherlink(request):
 
 def item(request):
     return HttpResponse('<h1>This is an item view</h1>')
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'food/detail.html', context)
