161223a8acccd7c146c49ba4a5b4360f9f8a190a
rr2x <79620740+rr2x@users.noreply.github.com>
2021-05-26 15:50:52 +0800

    Forms In Django (2), Authentication In Django (1)

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:item_id>', views.detail, name='detail'),
     path('add', views.create_item, name='create_item'),
     path('update/<int:id>', views.update_item, name='update_item'),
+    path('delete/<int:id>', views.delete_item, name='delete_item')
 ]
